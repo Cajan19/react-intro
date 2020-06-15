19254592c648dc6ea336df52fab8bc4801321b47
Carolin Janshen <caro.janshen@gmail.com>
2020-06-15 12:45:54 +0200

    Add minor adjustments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from "./components/Header"
+import List from "./components/List";
+
+
+const unicornAttributes = ["fluffig", "größenwahnsinnig", "bekloppt"];
+const animals = ["Jackal", "Wildebeest", "Lion", "Porcupine"]
 
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <List items={unicornAttributes}/>
+        <List items={animals}/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
     return (
         <header>
-            <h1>Warum ich Einhörner mag:</h1>
+            <h1>Dinge, die nicht sonderlich gut zusammenpassen:</h1>
         </header>
     )
 
